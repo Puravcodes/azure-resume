adba4f0cc070e87d95090319a18bf0889c6c051c
Purav <121781046+Puravcodes@users.noreply.github.com>
2024-11-05 16:15:56 +0530

    Update main.js

--- frontend/main.js
+++ frontend/main.js
@@ -2,7 +2,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
     getVisitCount();
 })
 
-const fucntionApi = '';
+const fucntionApi = 'http://localhost:7071/api/GetResumeCounter';
 
 const getVisitCount = () => {
     let count = 30;
